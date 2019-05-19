d826e55916507f1f9984dd7d11bc30926880a42b
smbduknow <smbduknow@gmail.com>
2019-05-19 16:52:20 +0300

    Minor search results redesign

--- app/src/main/java/me/smbduknow/vegandrinks/DrinkListAdapter.kt
+++ app/src/main/java/me/smbduknow/vegandrinks/DrinkListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_search_result.view.*
 import me.smbduknow.vegandrinks.data.model.Product
 
 class DrinkListAdapter : RecyclerView.Adapter<DrinkListAdapter.ViewHolder>() {
@@ -15,20 +16,22 @@ class DrinkListAdapter : RecyclerView.Adapter<DrinkListAdapter.ViewHolder>() {
     override fun getItemCount(): Int = items.size
 
     override fun onCreateViewHolder(parent: ViewGroup, pos: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search_result, parent, false)
         return ViewHolder(view) {
             onItemClickListener?.invoke(items[it])
         }
     }
 
     override fun onBindViewHolder(holder: ViewHolder, pos: Int) {
-        holder.cityTextView.text = "${items[pos].product_name} - ${items[pos].status}"
+        val item = items[pos]
+        holder.titleView.text = item.product_name
+        holder.statusText.text = item.status
     }
 
     class ViewHolder(itemView: View, clickListener: (pos: Int) -> Unit) : RecyclerView.ViewHolder(itemView) {
 
-        val cityTextView: TextView
-            get() = itemView as TextView
+        val titleView: TextView by lazy { itemView.title_text }
+        val statusText: TextView by lazy { itemView.status_text }
 
         init {
             itemView.setOnClickListener { clickListener(adapterPosition) }
