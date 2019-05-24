8077c96f8c834d71181d8644da6027a75cb992f6
smbduknow <smbduknow@gmail.com>
2019-05-25 01:06:08 +0300

    Redesign main screen

--- app/src/main/java/me/smbduknow/vegandrinks/MainActivity.kt
+++ app/src/main/java/me/smbduknow/vegandrinks/MainActivity.kt
@@ -1,6 +1,12 @@
 package me.smbduknow.vegandrinks
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -24,10 +30,19 @@ class MainActivity : AppCompatActivity() {
             adapter = suggestionAdapter
         }
 
-        btn_search.setOnClickListener { vm.onSubmit(search_edit.text?.toString() ?: "") }
+        search_edit.setOnEditorActionListener(object : TextView.OnEditorActionListener {
+            override fun onEditorAction(v: TextView, actionId: Int, event: KeyEvent?): Boolean {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    vm.onSubmit(v.text?.toString() ?: "")
+                    hideKeyboardFrom(v)
+                    return true
+                }
+                return false
+            }
+        })
 
         suggestionAdapter.onItemClickListener = { product ->
-            val company = vm.companies.find{ it.id == product.company_id }
+            val company = vm.companies.find { it.id == product.company_id }
             startActivity(ProductActivity.newIntent(this, product, company))
         }
 
@@ -35,5 +50,10 @@ class MainActivity : AppCompatActivity() {
             suggestionAdapter.items = suggestions
             suggestionAdapter.notifyDataSetChanged()
         }
+    }
+
+    fun hideKeyboardFrom(view: View) {
+        val imm = view.context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
