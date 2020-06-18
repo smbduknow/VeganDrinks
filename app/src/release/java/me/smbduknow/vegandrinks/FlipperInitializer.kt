2618a84511a60f5a4739862eb4bd8eca9e8579a4
smbduknow <smbduknow@gmail.com>
2020-06-18 22:00:30 +0300

    Fix Flipper imports for release

--- app/src/release/java/me/smbduknow/vegandrinks/FlipperInitializer.kt
+++ app/src/release/java/me/smbduknow/vegandrinks/FlipperInitializer.kt
@@ -1,11 +1,6 @@
 package me.smbduknow.vegandrinks
 
 import android.content.Context
-import com.facebook.flipper.android.AndroidFlipperClient
-import com.facebook.flipper.android.utils.FlipperUtils
-import com.facebook.flipper.plugins.inspector.DescriptorMapping
-import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
-import com.facebook.soloader.SoLoader
 import me.smbduknow.vegandrinks.BuildConfig
 
 object FlipperInitializer {
