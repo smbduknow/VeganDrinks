94ac4ceeb920a1c797c08c7f7c7f28400eec1077
smbduknow <smbduknow@gmail.com>
2021-01-28 00:28:38 +0100

    remove log

--- feature/search/src/main/java/me/smbduknow/vegandrinks/feature/search/SearchResultsFragment.kt
+++ feature/search/src/main/java/me/smbduknow/vegandrinks/feature/search/SearchResultsFragment.kt
@@ -2,7 +2,6 @@ package me.smbduknow.vegandrinks.feature.search
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
@@ -39,10 +38,7 @@ class SearchResultsFragment : Fragment(R.layout.fragment_search_results) {
         }
 
         vm.navigationChannel.consumeAsFlow()
-            .onEach {
-                Log.d("AAAAAAA", "aaaaaa")
-                navigateToDetails(it)
-            }
+            .onEach(::navigateToDetails)
             .launchIn(lifecycleScope)
 
     }
